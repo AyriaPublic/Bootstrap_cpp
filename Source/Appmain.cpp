190385d1ca3fc6dfe6cf93f4076637abb4f1c734
Convery <convery@localhost.localdomain>
2017-07-29 22:09:26 +0200

    Initial commit.

---
+++ Source/Appmain.cpp
@@ -0,0 +1,15 @@
+/*
+    Initial author: Convery (tcn@ayria.se)
+    Started: 29-07-2017
+    License: MIT
+    Notes:
+        Module entrypoint.
+*/
+
+#include "Stdinclude.h"
+
+int main()
+{
+
+    return 0;
+}
