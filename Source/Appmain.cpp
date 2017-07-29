63cdf855a0700c283caac5f1af51973530a0c080
Convery <convery@localhost.localdomain>
2017-07-29 23:04:03 +0200

    Added multi-arch building.

--- Source/Appmain.cpp
+++ Source/Appmain.cpp
@@ -8,8 +8,17 @@
 
 #include "Stdinclude.h"
 
-int main()
+// Standard entrypoint for ASCII / UTF8 systems.
+int main(int argc, char **argv)
 {
+    // Verify that we have received an app to start.
+    if(argc < 2)
+    {
+        // Log to the file and to stdout.
+        Infoprint("Failed to start, missing parameters.");
+        printf("Usage:\n%s Game [Startup arguments]\n", argv[0]);
+        return 1;
+    }
 
     return 0;
 }
